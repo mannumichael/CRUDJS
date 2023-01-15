558b34c6c5a76c28c733f266cd9b0ab067b04adf
mannumichael <mannu.besra272@gmail.com>
2023-01-15 18:45:40 +0530

    Connected MongoDB to Nodejs

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
-const express = require('express')
-const mongoose = require('mongoose')
+const express = require('express') ;
+const mongoose = require('mongoose') ;
+const dotenv = require('dotenv') ;
+const app = express() ;
+dotenv.config() ;
+const port = 9000 ;
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING,{
+  useNewUrlParser:true 
+}) ;
 
-const app = express()
+const db = mongoose.connection ;
 
+db.once('open',()=>{
+  console.log("Connected to MongoDB") ;
+})
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
-  })
+})
 
-app.listen(9000,()=>{
-    console.log('server started')
+app.listen(port,()=>{
+    console.log('Server started at port ${port}')
 })
