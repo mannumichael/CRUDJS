7eda96be957a11cf25a57123e4e3dff5e2d4ca70
Preetam Singh Dabi <preetamdabi1812@gmail.com>
2023-01-15 19:01:13 +0530

    Added model schema

---
+++ model/customer.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+
+
+var currentdate = new Date(); 
+var datetime = currentdate.getDate() + "/"
+                + (currentdate.getMonth()+1)  + "/" 
+                + currentdate.getFullYear() + " @ "  
+                + currentdate.getHours() + ":"  
+                + currentdate.getMinutes() + ":" 
+                + currentdate.getSeconds();
+
+const customerSchema = new mongoose.Schema({
+    name: {
+        type : string,
+        required: true
+    },
+    city: {
+        type: string,
+        required: true
+    },
+    postalcode: {
+        type : string,
+        required: true
+    },
+    date: {
+        type: string,
+        required: true,
+        default : datetime 
+    }
+})
+module.exports = customerSchema
