3044df64f2dab7a1e43e72a3395809d791b3fab3
mannumichael <mannu.besra272@gmail.com>
2023-01-16 19:32:19 +0530

    added Validation in Schema

--- model/customer.js
+++ model/customer.js
@@ -12,15 +12,31 @@ var datetime = currentdate.getDate() + "/"
 const customerSchema = new mongoose.Schema({
     name: {
         type : String,
-        required: true
+        required: true,
+        minlength: 2,
+        maxlength: 50
     },
     city: {
         type: String,
-        required: true
+        required: true,
+        validate: {
+            validator: function(value) {
+                return /^[a-zA-Z\s]*$/.test(value);
+            },
+            message: "Invalid city format"
+        },
+        minlength: 2,
+        maxlength: 50
     },
     postalcode: {
         type : String,
-        required: true
+        required: true,
+        validate: {
+            validator: function(value) {
+                return /^\d{6}$/.test(value);
+            },
+            message: "Invalid postal code format, should be 6 digits"
+        }
     },
     date: {
         type: String,
@@ -30,5 +46,7 @@ const customerSchema = new mongoose.Schema({
 },{
     versionKey: false
 })
+                
+                
 
 module.exports = mongoose.model('customers',customerSchema)
